dbf1efb21c4f09b3b622fb7c3c64763cea7fd3ca
munachiso <ifechukwumunachiso@gmail.com>
2022-05-26 01:45:39 -0700

    first commit

---
+++ anagram.py
@@ -0,0 +1,16 @@
+# Check if two words are anagrams 
+# Example:
+# find_anagrams("hello", "check") --> False
+# find_anagrams("below", "elbow") --> True
+
+
+def find_anagrams(word, anagram):
+    # [assignment] Add your code here
+    if (sorted(word) == sorted(anagram)):
+        return True
+    else:
+        return False
+
+#testing
+find_anagrams("hello", "check") 
+find_anagrams("below", "elbow")
